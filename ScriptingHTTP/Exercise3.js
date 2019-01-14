23abcd914c987d04a1ae13b81f61661d3933c53f
Sam Soheylee <ssoheyleearasi@gmail.com>
2019-01-14 21:30:21 +0000

    Exercise 3

---
+++ ScriptingHTTP/Exercise3.js
@@ -0,0 +1,12 @@
+function getAndPrintHTML (options) {
+
+  options = requestOptions;
+  console.log(options)
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML();

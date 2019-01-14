c9880d4774f3cc3d6657af47e26bf6b0c4a5cdae
Sam Soheylee <ssoheyleearasi@gmail.com>
2019-01-14 22:54:44 +0000

    pushing E3 again

--- ScriptingHTTP/Exercise3.js
+++ ScriptingHTTP/Exercise3.js
@@ -1,7 +1,22 @@
+var https = require('https');
+
 function getAndPrintHTML (options) {
 
-  options = requestOptions;
-  console.log(options)
+  https.get(options, function (response){
+
+    var buffer = "";
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data){
+      buffer = buffer.concat(data);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log(buffer);
+    });
+  });
 }
 
 var requestOptions = {
@@ -9,4 +24,4 @@ var requestOptions = {
   path: '/http-examples/step3.html'
 };
 
-getAndPrintHTML();
+getAndPrintHTML(requestOptions);
